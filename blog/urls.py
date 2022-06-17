c8f5a7b57cb2cbe61d3f76f11bc2114c7b08a4ef
artur <khalikart@gmail.com>
2022-06-17 15:17:21 +0500

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import post_detail, PostListView, post_share,\
+post_list, post_search
+from .feeds import LatestPostsFeed
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>',
+        post_detail, name='post_detail'),
+    path('<int:post_id>/share/', post_share, name='post_share'),
+    path('tag/<slug:tag_slug>/', post_list, name='post_list_by_tag'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('search/', post_search, name='post_search')
+]
